5a6e7f305d91969204ae957dd530a010bfe3e6ad
Wong Keng <kaira428@gmail.com>
2022-08-30 15:50:47 +0800

    add useer email when login

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,6 +23,7 @@ export default function Navbar() {
               <a>Guides</a>
             </Link>
           </li>
+          {user && <li>{user.email}</li>}
           {user && <li onClick={logout} className='btn'>Logout</li>}
           {!user && <li onClick={login} className='btn'>Login/Signup</li>}
         </ul>
